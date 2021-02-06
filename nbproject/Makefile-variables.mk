49babe8751577bf70e8d3bd0af2062897c62bbbe
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2021-02-06 08:04:38 +0100

    Add readme. Upgrade project to MPLABX V5.40 and XC8 V2.20.

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=SPRD.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/SPRD.X.production.hex
+CND_ARTIFACT_NAME_default=SPRD.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SPRD.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=sprd.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/sprd.x.tar
+CND_PACKAGE_NAME_default=sprd.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/sprd.tar
--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/SPRD.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=SPRD.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=sprd.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/SPRD.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=SPRD.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=sprd/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/sprd.x/bin
+makeDirectory ${TMPDIR}/sprd/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/sprd.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/sprd.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/sprd.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/sprd.tar *
 checkReturnCode
 
 # Cleanup
