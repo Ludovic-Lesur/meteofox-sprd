8987aa0cd3a68c74b4c818546e26b9f2b624cc74
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2023-03-11 14:14:02 +0100

    Rename project.

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=SPRD.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/SPRD.production.hex
+CND_ARTIFACT_NAME_default=sprd.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/sprd.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
 CND_PACKAGE_NAME_default=sprd.tar
 CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/sprd.tar
